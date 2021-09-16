2a04f12699042df6354c2c9eac18f879698c9aa7
Lion69 <lion69@mail.ru>
2021-09-16 13:48:48 +0300

    Examples and notes on Youtube MIPT Python sicp lectures.

--- foo/range4iterate.py
+++ foo/range4iterate.py
@@ -1,3 +1,23 @@
+"""
 A = [1, 2, 3, 4, 5]
 for k in range(5):
     print(k)
+
+B = [0] * 10
+
+print("-=-=-=-=-=-=-=-=-")
+
+for k in range(10):
+   print(B[k])
+"""
+
+A = [0]*1000
+top = 0
+x = int(input())
+while x != 0:
+    A[top] = x
+    top += 1
+    x = int(input())
+
+for k in range (top - 1, -1, -1):
+    print(A[k])
