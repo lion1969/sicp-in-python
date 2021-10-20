60cff1f4a9fdf6b9742e1acaa7aaf831c169ad02
Lion69 <lion69@mail.ru>
2021-10-20 16:20:19 +0300

    Examples and notes on Youtube MIPT Python sicp lectures.

--- sicp_mipt_youtube/lec06/sort_O_N2.py
+++ sicp_mipt_youtube/lec06/sort_O_N2.py
@@ -1,16 +1,36 @@
 def insert_sort(A):
     """ Сортировка списка А вставками """
-    pass
+    N = len(A)
+    for top in range(1, N):
+        k = top
+        while k > 0 and A[k - 1] > A[k]:
+            A[k], A[k - 1] = A[k - 1], A[k]
+            k -= 1
 
 
 def choise_sort(A):
     """ Сортировка списка А выбором"""
-    pass
+    N = len(A)
+    for pos in range(0, N - 1):
+        for k in range(pos + 1, N):
+            if A[k] < A[pos]:
+                A[k], A[pos] = A[pos], A[k]
+
 
 def bubble_sort(A):
     """ Сортировка списка А методом пузырька """
+    N = len(A)
+    for bypass in range(1,N):
+        for k in range(0, N - bypass):
+            if A[k] > A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
+
+
+
 
 def test_sort(sort_algorithm):
+    print("Тестируем: ", sort_algorithm.__doc__)
+
     print("testcase #1: ", end="")
     A = [4, 2, 5, 1, 3]
     A_sorted = [1, 2, 3, 4, 5]
@@ -18,17 +38,19 @@ def test_sort(sort_algorithm):
     print("OK" if A == A_sorted else "Fail")
 
     print("testcase #2: ", end="")
-    A = [4, 2, 5, 1, 3]
-    A_sorted = [1, 2, 3, 4, 5]
+    A = list(range(10, 20)) + list(range(0, 10))
+    A_sorted = list(range(0, 20))
     sort_algorithm(A)
     print("OK" if A == A_sorted else "Fail")
 
-    print("testcase #1: ", end="")
-    A = [4, 2, 5, 1, 3]
-    A_sorted = [1, 2, 3, 4, 5]
+    print("testcase #3: ", end="")
+    A = [4, 2, 4, 2, 3]
+    A_sorted = [2, 2, 3, 4, 4]
     sort_algorithm(A)
     print("OK" if A == A_sorted else "Fail")
 
-if name == "__main__":
-    test_sort()
 
+if __name__ == "__main__":
+    test_sort(insert_sort)
+    test_sort(choise_sort)
+    test_sort(bubble_sort)
