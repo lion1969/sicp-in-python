884d3e06d4061c80a6ee5e4bc50f8aa2068d8c1d
Lion69 <lion69@mail.ru>
2021-10-26 12:29:39 +0300

    Examples and notes on Youtube MIPT Python sicp lectures. lec07. Recursive function, graphical examples. using window.wait_window() to stop closing window in Pycharm

--- sicp_mipt_youtube/lec07/recurse_graph_example.py
+++ sicp_mipt_youtube/lec07/recurse_graph_example.py
@@ -16,6 +16,7 @@ def fractal_rectangle(A, B, C, D, deep=10):
     fractal_rectangle(A1, B1, C1, D1, deep - 1)
 
 
-fractal_rectangle((100, 100), (500, 100), (500, 500), (100, 500))
-
-abc = input()
+fractal_rectangle((100, 100), (500, 100), (500, 500), (100, 500), 51)
+#window.getMouse()
+window.wait_window()
+window.close()
