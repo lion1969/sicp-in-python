3f5918830629ae2b8f4c8dd4da187dad90c58c41
Lion69 <lion69@mail.ru>
2021-10-26 16:55:19 +0300

    Examples and notes on Youtube MIPT Python sicp lectures. lec07. Recursive function, graphical examples... using window.wait_window() to stop closing window in Pycharm

--- sicp_mipt_youtube/lec07/gcd_example.py
+++ sicp_mipt_youtube/lec07/gcd_example.py
@@ -1,6 +1,10 @@
-def gcd(a: int, b: int):
+def gcd_substraction(a: int, b: int) -> int:
     """
-    Return Greatest Common Devisor
+    Return Greatest Common Devisor.
+    GCD =:
+    a, if b = b
+    gcd(a - b, b) if a > b
+    gcd(a, b - a) if b > a
     :param a: int
     :param b: int
     :return: int
@@ -9,9 +13,36 @@ def gcd(a: int, b: int):
     if a == b:
         return a
     elif a > b:
-        return gcd(a - b, b)
-    else: # a < b
-        return gcd(a, b - a)
+        return gcd_substraction(a - b, b)
+    else:  # a < b
+        return gcd_substraction(a, b - a)
 
 
+def gcd(a: int, b: int) -> int:
+    """
+    GCD(a, b) == GCD(b, a % b)
+    Return Greatest Common Devisor.
+    GCD =:
+    a, if b = 0
+    gcd(a - b, b) if a > b
+    gcd(a, b - a) if b > a
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+def gcd_cool(a: int, b: int) -> int:
+    """
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    return a if b == 0 else gcd_cool(b, a % b)
+
+print(gcd_substraction(36, 60))
 print(gcd(36, 60))
+print(gcd_cool(36, 60))
