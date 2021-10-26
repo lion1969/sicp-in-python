cf9eeec671c1a96a549333202f47e34a554bd372
Lion69 <lion69@mail.ru>
2021-10-26 17:07:09 +0300

    Examples and notes on Youtube MIPT Python sicp lectures. lec07. Recursive function, graphical examples... using window.wait_window() to stop closing window in Pycharm

---
+++ sicp_mipt_youtube/lec07/power_natural_number.py
@@ -0,0 +1,15 @@
+"""
+a**n
+
+pow(a,n) =
+        1, if n = 0
+        pow(a, n-1)*a, n > 0
+"""
+
+
+def pow(a: int, n: int) -> int:
+    assert n >= 0 or a >= 0, "Agrh! Ouch!"
+    return 1 if n == 0 else pow(a, n - 1) * a
+
+
+print(pow(2, 4))
