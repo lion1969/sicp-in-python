64747a94e95dd7ed396cf37fb201c3ca60056f95
Lion69 <lion69@mail.ru>
2021-12-13 12:36:21 +0300

    Examples and notes on Youtube MIPT Python sicp lectures. lec08. Recurrent algorithm for permutations.

--- sicp_mipt_youtube/lec06/list_comprehensions.py
+++ sicp_mipt_youtube/lec06/list_comprehensions.py
@@ -5,12 +5,11 @@ print(B)
 # squares
 # Redefine array A!!!!
 A = [x*x for x in A]
+A.reverse()
 print(A)
-
 First = [1,2]
 Second = [3,4]
 Third = [5,6]
-
 # Merging a 2D array in Python into one string with List Comprehension
 A = [item for lst in (First, Second, Third) for item in lst]
 print(A)
--- sicp_mipt_youtube/lec09/fast_sortings.py
+++ sicp_mipt_youtube/lec09/fast_sortings.py
@@ -2,9 +2,7 @@ def noar_sort(A):
     if len(A) < 1:
         return
     barrier = A[0]
-    L = []
-    M = []
-    R = []
+    L, R, M = [], [], []
     for x in A:
         if x < barrier:
             L.append(x)
@@ -14,9 +12,14 @@ def noar_sort(A):
             R.append(x)
     noar_sort(L)
     noar_sort(R)
+    # A = [item for lst in (L, M, R) for item in lst]
+    i = 0
+    for lst in (L, M, R):
+        for element in lst:
+            A[i] = element
+            i += 1
 
-AA = [0,1,2,3,4,5]
-LL = ['a', 'b']
-MM = ['c', 'd']
-RR = ['e','f']
-AA =
+
+A = [1, 7, 2, 4, 5, 4, 1, 3, 6, 2, 1]
+noar_sort(A)
+print(A)
